2088f189627678b14652eae802897f381667ddd4
JulianGarzon22 <bryangarzon22@gmail.com>
2019-03-05 01:55:59 -0500

    Three buldings (with errors)

--- App.js
+++ App.js
@@ -17,24 +17,18 @@ import {
   TouchableHighlight,
 } from 'react-native';
 
-import {
-  ViroVRSceneNavigator,
-  ViroARSceneNavigator
-} from 'react-viro';
+import { ViroARSceneNavigator } from 'react-viro';
 
 /*
  TODO: Insert your API key below
  */
 var sharedProps = {
-  apiKey:"DB41029D-6271-4035-AD9E-3F90339CFA91",
+  apiKey: "DB41029D-6271-4035-AD9E-3F90339CFA91",
 }
 
-// Sets the default scene you want for AR and VR
-var InitialARScene = require('./js/HelloWorldSceneAR');
-var InitialVRScene = require('./js/HelloWorldScene');
+var InitialARScene = require('./js/SceneAR');
 
 var UNSET = "UNSET";
-var VR_NAVIGATOR_TYPE = "VR";
 var AR_NAVIGATOR_TYPE = "AR";
 
 // This determines which type of experience to launch in, or UNSET, if the user should
@@ -46,12 +40,11 @@ export default class ViroSample extends Component {
     super();
 
     this.state = {
-      navigatorType : defaultNavigatorType,
-      sharedProps : sharedProps
+      navigatorType: defaultNavigatorType,
+      sharedProps: sharedProps
     }
     this._getExperienceSelector = this._getExperienceSelector.bind(this);
     this._getARNavigator = this._getARNavigator.bind(this);
-    this._getVRNavigator = this._getVRNavigator.bind(this);
     this._getExperienceButtonOnPress = this._getExperienceButtonOnPress.bind(this);
     this._exitViro = this._exitViro.bind(this);
   }
@@ -61,8 +54,6 @@ export default class ViroSample extends Component {
   render() {
     if (this.state.navigatorType == UNSET) {
       return this._getExperienceSelector();
-    } else if (this.state.navigatorType == VR_NAVIGATOR_TYPE) {
-      return this._getVRNavigator();
     } else if (this.state.navigatorType == AR_NAVIGATOR_TYPE) {
       return this._getARNavigator();
     }
@@ -75,21 +66,14 @@ export default class ViroSample extends Component {
         <View style={localStyles.inner} >
 
           <Text style={localStyles.titleText}>
-            Choose your desired experience:
+            Inicia la experiencia
           </Text>
 
           <TouchableHighlight style={localStyles.buttons}
             onPress={this._getExperienceButtonOnPress(AR_NAVIGATOR_TYPE)}
             underlayColor={'#68a0ff'} >
 
-            <Text style={localStyles.buttonText}>AR</Text>
-          </TouchableHighlight>
-
-          <TouchableHighlight style={localStyles.buttons}
-            onPress={this._getExperienceButtonOnPress(VR_NAVIGATOR_TYPE)}
-            underlayColor={'#68a0ff'} >
-
-            <Text style={localStyles.buttonText}>VR</Text>
+            <Text style={localStyles.buttonText}>INICIAR</Text>
           </TouchableHighlight>
         </View>
       </View>
@@ -100,15 +84,7 @@ export default class ViroSample extends Component {
   _getARNavigator() {
     return (
       <ViroARSceneNavigator {...this.state.sharedProps}
-        initialScene={{scene: InitialARScene}} />
-    );
-  }
-  
-  // Returns the ViroSceneNavigator which will start the VR experience
-  _getVRNavigator() {
-    return (
-      <ViroVRSceneNavigator {...this.state.sharedProps}
-        initialScene={{scene: InitialVRScene}} onExitViro={this._exitViro}/>
+        initialScene={{ scene: InitialARScene }} />
     );
   }
 
@@ -117,7 +93,7 @@ export default class ViroSample extends Component {
   _getExperienceButtonOnPress(navigatorType) {
     return () => {
       this.setState({
-        navigatorType : navigatorType
+        navigatorType: navigatorType
       })
     }
   }
@@ -125,60 +101,60 @@ export default class ViroSample extends Component {
   // This function "exits" Viro by setting the navigatorType to UNSET.
   _exitViro() {
     this.setState({
-      navigatorType : UNSET
+      navigatorType: UNSET
     })
   }
 }
 
 var localStyles = StyleSheet.create({
-  viroContainer :{
-    flex : 1,
-    backgroundColor: "black",
+  viroContainer: {
+    flex: 1,
+    backgroundColor: "#707070",
   },
-  outer : {
-    flex : 1,
+  outer: {
+    flex: 1,
     flexDirection: 'row',
-    alignItems:'center',
-    backgroundColor: "black",
+    alignItems: 'center',
+    backgroundColor: "#707070",
   },
   inner: {
-    flex : 1,
+    flex: 1,
     flexDirection: 'column',
-    alignItems:'center',
-    backgroundColor: "black",
+    alignItems: 'center',
+    backgroundColor: "#707070",
   },
   titleText: {
     paddingTop: 30,
     paddingBottom: 20,
-    color:'#fff',
-    textAlign:'center',
-    fontSize : 25
+    color: '#fff',
+    textAlign: 'center',
+    fontSize: 25
   },
   buttonText: {
-    color:'#fff',
-    textAlign:'center',
-    fontSize : 20
+    color: '#fff',
+    textAlign: 'center',
+    fontSize: 20
   },
-  buttons : {
+  buttons: {
     height: 80,
     width: 150,
-    paddingTop:20,
-    paddingBottom:20,
+    paddingTop: 20,
+    paddingBottom: 20,
     marginTop: 10,
     marginBottom: 10,
-    backgroundColor:'#68a0cf',
+    backgroundColor: '#94B43B',
     borderRadius: 10,
     borderWidth: 1,
     borderColor: '#fff',
   },
-  exitButton : {
+  exitButton: {
     height: 50,
     width: 100,
-    paddingTop:10,
-    paddingBottom:10,
+    paddingTop: 10,
+    paddingBottom: 10,
     marginTop: 10,
     marginBottom: 10,
-    backgroundColor:'#68a0cf',
+    backgroundColor: '#94B43B',
     borderRadius: 10,
     borderWidth: 1,
     borderColor: '#fff',
---
+++ js/SceneAR.js
@@ -0,0 +1,147 @@
+'use strict';
+import React, { Component } from 'react';
+import { StyleSheet, NativeEventEmitter, DeviceEventEmitter, AsyncStorage, PermissionsAndroid } from 'react-native';
+import ReactNativeHeading from '@zsajjad/react-native-heading';
+import Geolocation from 'react-native-geolocation-service'
+import {
+  ViroARSceneNavigator,
+  ViroARScene,
+  ViroText,
+  ViroConstants,
+  ViroImage,
+  ViroARPlane,
+  ViroBox,
+} from 'react-viro';
+import RNSimpleCompass from 'react-native-simple-compass';
+const degree_update_rate = 3; // Number of degrees changed before the callback is triggered
+export default class SceneAR extends Component {
+  async requestLocationPermission() {
+    try {
+      const granted = await PermissionsAndroid.request(
+        PermissionsAndroid.PERMISSIONS.ACCESS_FINE_LOCATION
+      )
+      if (granted === PermissionsAndroid.RESULTS.GRANTED) {
+        //console.log("You can use the location")
+        //alert("You can use the location");
+        Geolocation.getCurrentPosition((Degree = this.state.Degree) => { //change to position if degree doesnt work
+          this.setState({
+            latitude: Degree.coords.latitude,
+            longitude: Degree.coords.longitude,
+            error: null,
+            string: String(Degree.coords.latitude)
+          });
+        }, (error) => this.setState({ error: error.message }),
+          { enableHighAccuracy: true }
+        )
+      } else {
+        //console.log("location permission denied")
+        alert("Location permission denied");
+      }
+      return granted;
+    } catch (err) {
+      console.warn(err)
+    }
+  }
+  constructor() {
+    super();
+    this.requestLocationPermission = this.requestLocationPermission.bind(this);
+    // Set initial state here
+    this.state = {
+      text: "Inicializando AR...",
+      latitude: 0,
+      longitude: 0,
+      cytX: 0,
+      cytZ: 0,
+      quimicaX: 0,
+      quimicaZ: 0,
+      aulasX: 0,
+      aulasZ: 0,
+      headingAngle: 0,
+      headAngle: 0
+    };
+    RNSimpleCompass.start(degree_update_rate, (degree) => {
+      this.setState({ Degree: degree });
+      RNSimpleCompass.stop();
+    });
+    // bind 'this' to functions   
+    this._onInitialized = this._onInitialized.bind(this);
+    this._latLongToMerc = this._latLongToMerc.bind(this);
+    this._transformPointToAR = this._transformPointToAR.bind(this);
+  }
+  updateHead() {
+    AsyncStorage.setItem('headstart', JSON.stringify(this.state.headingAngle))
+  }
+  componentDidMount() {
+    this.requestLocationPermission();
+    ReactNativeHeading.start(1)
+      .then(didStart => {
+        this.setState({
+          headingIsSupported: didStart,
+        })
+      })
+    var flag = 0
+    DeviceEventEmitter.addListener('headingUpdated', heading => {
+      this.setState({ headingAngle: heading })
+      //console.log('heading State',heading)
+      if (flag == 0) {
+        this.updateHead()
+      }
+      flag = 1
+    });
+    this.updateHead()
+  }
+  componentWillUnmount() {
+    ReactNativeHeading.stop();
+    DeviceEventEmitter.removeAllListeners('headingUpdated');
+  }
+  render() {
+    return (<ViroARScene onTrackingUpdated={this._onInitialized}>
+      <ViroText text="CyT" scale={[4, 4, 4]} transformBehaviors={["billboard"]} position={[this.state.cytX, 0, this.state.cytZ]} style={styles.helloWorldTextStyle} />
+      <ViroText text="Química" scale={[4, 4, 4]} transformBehaviors={["billboard"]} position={[this.state.quimicaX, 0, this.state.quimicaZ]} style={styles.helloWorldTextStyle} />
+      <ViroText text="Aulas" scale={[4, 4, 4]} transformBehaviors={["billboard"]} position={[this.state.aulasX, 0, this.state.aulasZ]} style={styles.helloWorldTextStyle} />
+    </ViroARScene>);
+  }
+  _onInitialized() {
+    var cyt = this._transformPointToAR(4.6381390, -74.0845033);
+    var qumica = this._transformPointToAR(4.6376711, -74.0836540);
+    var aulas = this._transformPointToAR(4.6385059, -74.0835930);
+    this.setState({
+      cytX: cyt.x,
+      cytZ: cyt.z,
+      quimicaX: qumica.x,
+      quimicaZ: qumica.z,
+      aulasX: aulas.x,
+      aulasZ: aulas.z,
+      text: "AR Init called."
+    });
+  }
+  _latLongToMerc(lat_deg, lon_deg) {
+    var lon_rad = (lon_deg / 180.0 * Math.PI)
+    var lat_rad = (lat_deg / 180.0 * Math.PI)
+    var sm_a = 637813.70
+    var xmeters = sm_a * lon_rad
+    var ymeters = sm_a * Math.log((Math.sin(lat_rad) + 1) / Math.cos(lat_rad))
+    return ({ x: xmeters, y: ymeters });
+  }
+  _transformPointToAR(lat, long) {
+    AsyncStorage.getItem('headstart')
+      .then(res => { console.log('HeadAngle', res), this.setState({ headAngle: res }) })
+    var objPoint = this._latLongToMerc(lat, long);
+    var devicePoint = this._latLongToMerc(this.state.latitude, this.state.longitude);
+    var objFinalPosZ = (objPoint.y - devicePoint.y);
+    var objFinalPosX = (objPoint.x - devicePoint.x);
+
+    return ({ x: objFinalPosX, z: -objFinalPosZ });
+  }
+}
+var styles = StyleSheet.create({
+  helloWorldTextStyle: {
+    fontFamily: 'Arial',
+    fontSize: 30,
+    color: '#d30606',
+    textAlignVertical: 'center',
+    textAlign: 'center',
+  },
+});
+
+module.exports = SceneAR; 
